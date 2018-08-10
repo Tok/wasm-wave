005f77e190e5b9902c40dabba468ce80cf58e147
Tok <lsteiger4@gmail.com>
2018-08-10 22:50:14 +0200

    Fill screen calculations.

--- src/main/kotlin/View.kt
+++ src/main/kotlin/View.kt
@@ -10,26 +10,39 @@ import util.WaveCalc
 class View(private val can: Canvas) : Layout(can.getBoundingClientRect()) {
     private val context = can.getContext("2d")
 
-    private fun drawWaves(i: Int) = with (Model) {
-        context.beginPath()
+    private fun drawWaves(i: Int) = with(Model) {
+        val topOffset = ((h % resolution) / 2).toInt()
+        val leftOffset = ((w % resolution) / 2).toInt()
         val t = i * velocity()
-        val yRange = 0..(h - resolution)
-        val xRange = 0..(w - resolution)
+        val yRange = topOffset..(h - resolution)
+        val xRange = leftOffset..(w - resolution)
+        context.beginPath()
         for (y in yRange step resolution) {
             for (x in xRange step resolution) {
-                drawPixel(x, y, t)
+                drawPixel(x, y, t, topOffset, leftOffset)
             }
         }
     }
 
-    private fun drawPixel(x: Int, y: Int, t: Double) {
+    private fun drawPixel(x: Int, y: Int, t: Double,
+                          topOffset: Int = 0, leftOffset: Int = 0) = with(Model) {
         val pos = Pos(x, y)
-        val waves: List<Complex> = Model.particles.map {
+        val waves: List<Complex> = particles.map {
             WaveCalc.calc(pos, it.pos, t.toDouble(), frequency(), intensity())
         }
         val waveSum = waves.fold(Complex.ZERO) { sum, el -> sum + el }
         context.fillStyle = ColorUtil.getColor(waveSum)
-        context.fillRect(x, y, Model.resolution, Model.resolution)
+        val (xx, xRes) = when {
+            x <= leftOffset -> (x - leftOffset) to (resolution + leftOffset) //first col
+            x >= (w - (2 * resolution)) -> x to (resolution + leftOffset) //last col
+            else -> x to resolution
+        }
+        val (yy, yRes) = when {
+            y <= topOffset -> (y - topOffset) to (resolution + topOffset) //first row
+            y > (h - (2 * resolution)) -> y to (resolution + topOffset) //last row
+            else -> y to resolution
+        }
+        context.fillRect(xx, yy, xRes, yRes)
     }
 
     private fun drawParticles() {
@@ -51,7 +64,7 @@ class View(private val can: Canvas) : Layout(can.getBoundingClientRect()) {
             fillStyle = Style.fontColor
             fillText(text, 2, topOffset, 100)
         }
-        with (Model) {
+        with(Model) {
             print("FPS: ${calcFps()}", 10)
             print("Pixels: $pixelCount", 20)
             print("Tick: $tick", 30)
